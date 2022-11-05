75fab96e659a3f22ab7fe43ed019392987fde9f6
memo <alexis45645@gmail.com>
2022-11-04 22:47:57 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {
+
+	public static void main(String[] args) {//q69
+		// TODO Auto-generated method stub
+		System.out.println(mySqrt(8));
+	}
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
